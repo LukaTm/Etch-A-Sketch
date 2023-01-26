1e5fcfeb92e0d44cdd8a36f90d04885c76be66d0
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-01-26 17:09:44 +0200

    Add Button to choose size

--- etch.js
+++ etch.js
@@ -1,29 +1,30 @@
 const container = document.getElementById("start");
-const gridSize = document.getElementById("grid-size");
-const mouseHover = document.getElementsByClassName('grid-square');
+const button = document.getElementsByClassName("grid-size")[0];
 
 
-const input = prompt('Grid size')
+
+button.addEventListener("click", function() {
+    input = prompt("Grid Size:");
+    container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+    for (let i = 0; i < gridSizeInput(); i++) {
+        let div = document.createElement("div");
+        div.classList.add("grid-square");
+        container.appendChild(div);
+      }
+    mouseHover = document.getElementsByClassName('grid-square');
+    for (let i = 0; i < mouseHover.length; i++) {
+        mouseHover[i].addEventListener('mouseover', () => color(mouseHover[i]));
+    }
+});
 
 
-container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${input}, 1fr)`;
 function gridSizeInput(){
     return input * input;
 }
-
-
-for (let i = 0; i < gridSizeInput(); i++) {
-  let div = document.createElement("div");
-  div.classList.add("grid-square");
-  container.appendChild(div);
-}
-
 
 function color(square){
     return square.style.backgroundColor = 'red'
 }
 
-for (let i = 0; i < mouseHover.length; i++) {
-    mouseHover[i].addEventListener('mouseover', () => color(mouseHover[i]));
-}
+
