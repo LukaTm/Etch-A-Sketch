be83aeb4d108893103c54612bceb2310466b7ba4
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-01-26 15:43:54 +0200

    Grid size based on user input

--- etch.js
+++ etch.js
@@ -1,7 +1,29 @@
 const container = document.getElementById("start");
+const gridSize = document.getElementById("grid-size");
+const mouseHover = document.getElementsByClassName('grid-square');
 
-for (let i = 0; i < 256; i++) {
+
+const input = prompt('Grid size')
+
+
+container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+function gridSizeInput(){
+    return input * input;
+}
+
+
+for (let i = 0; i < gridSizeInput(); i++) {
   let div = document.createElement("div");
   div.classList.add("grid-square");
   container.appendChild(div);
+}
+
+
+function color(square){
+    return square.style.backgroundColor = 'red'
+}
+
+for (let i = 0; i < mouseHover.length; i++) {
+    mouseHover[i].addEventListener('mouseover', () => color(mouseHover[i]));
 }
