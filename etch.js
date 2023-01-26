c8bde3fb551fc5846e6bdc6fa7d2cdc594501065
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-01-26 17:38:04 +0200

    Add Random color for each square

--- etch.js
+++ etch.js
@@ -2,17 +2,18 @@ const container = document.getElementById("start");
 const button = document.getElementsByClassName("grid-size")[0];
 
 
-
 button.addEventListener("click", function() {
     input = prompt("Grid Size:");
     container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+    
     for (let i = 0; i < gridSizeInput(); i++) {
         let div = document.createElement("div");
         div.classList.add("grid-square");
         container.appendChild(div);
-      }
-    mouseHover = document.getElementsByClassName('grid-square');
+    }
+
+    let mouseHover = document.getElementsByClassName('grid-square');
     for (let i = 0; i < mouseHover.length; i++) {
         mouseHover[i].addEventListener('mouseover', () => color(mouseHover[i]));
     }
@@ -23,8 +24,14 @@ function gridSizeInput(){
     return input * input;
 }
 
+
+// Random Color
 function color(square){
-    return square.style.backgroundColor = 'red'
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    RGB_COLOR = `rgb(${randomR}, ${randomG}, ${randomB})`
+    return square.style.backgroundColor = RGB_COLOR;
 }
 
 
